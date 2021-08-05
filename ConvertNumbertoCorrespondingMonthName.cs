258559509fc0d6627e94196be3eebcf9aeb78f4f
Kotov584 <85634618+Kotov584@users.noreply.github.com>
2021-08-05 17:14:40 +0300

    Update ConvertNumbertoCorrespondingMonthName.cs

--- ConvertNumbertoCorrespondingMonthName.cs
+++ ConvertNumbertoCorrespondingMonthName.cs
@@ -2,7 +2,7 @@ public class Program
 {
     public static string MonthName(int num) 
     {
-			string[] months = {"", "January","February","March","April","May","June","July","August","September","October","November","December"};
+	string[] months = {"", "January","February","March","April","May","June","July","August","September","October","November","December"};
     	return months[num];
-		}
+    }
 }
